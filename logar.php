5d3e2be404158ed6ca5d311d209753b381c0317c
Alef Felix <alef.developerweb@gmail.com>
2018-02-22 14:38:32 -0300

    Segundo dia

--- logar.php
+++ logar.php
@@ -11,7 +11,29 @@
             teste
         </aside>
         <section>
-            teste
+            <p class='destaque'>teste</p>
         </section>
+
+        <div class="msg">
+            teste 1
+        </div>
+
+        <div class="msg msg-error">
+            teste 2
+        </div>
+
+        <div class="msg msg-alert">
+            teste 3
+        </div>
+        <a href="#">teste 4</a>
+        <div class="error">
+            teste 5
+        </div>
+        <div class="attention">
+            teste 6
+        </div>
+        <div class="seriousError">
+            teste 7
+        </div>
     </body>
 </html>
